3cbc70fa53f24369f81de50db7482cbdb7b52a5b
Mohib Khan <khanmohib2001@gmail.com>
2025-11-28 16:28:22 +0500

    Imported font family: Raleway

--- src/pages/Categories/Mens_collection.jsx
+++ src/pages/Categories/Mens_collection.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Mens_collection = () => {
   return (
-    <div>Mens_collection</div>
-  )
-}
+    <div>
+      <div className="flex w-full flex-col justify-center items-center">
+        <h1 className="text-5xl text-emerald-950 font-extrabold font-primary-extraBold">Men's Collection</h1>
+        <span class="text-gray-600 text-sm mt-2 block font-primary-medium font-extrabold">
+          Explore the latest styles and trends in men's fashion.
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default Mens_collection
+export default Mens_collection;
